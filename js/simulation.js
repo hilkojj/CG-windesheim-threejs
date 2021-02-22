1153afcfcc9143ad8cd7ee7f0885d9edae493fef
hilkojj <hilkojj@outlook.com>
2021-02-22 15:45:27 +0100

    grass and stuff

--- js/simulation.js
+++ js/simulation.js
@@ -35,11 +35,16 @@ function init() {
     cube.position.x = 1;
     scene.add(cube);
 
-    function loadModels(file) {
+    function loadModel(file, x, y, z) {
         const loader = new THREE.GLTFLoader();
         loader.load(file, function (gltf) {
 
-            scene.add(gltf.scene);
+            const model = gltf.scene.children[0];
+            model.position.x = x;
+            model.position.y = y;
+            model.position.z = z;
+
+            scene.add(model);
 
         }, undefined, function (error) {
 
@@ -48,8 +53,8 @@ function init() {
         });
     }
 
-    loadModels("models/zamek.glb")
-    loadModels("models/martini_toren.glb")
+    loadModel("models/zamek.glb", 10, -1, 0)          // a castle in Poland
+    loadModel("models/martini_toren.glb", 0, -1, 10)  // a tower in Groningen
 
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1.);
@@ -71,6 +76,28 @@ function init() {
             './models/textures/skyboxsun45deg/skyrender0002.png'    // neg-z
         ]);
         scene.background = texture;
+    }
+    // Adding fog:
+    {
+        const color = 0x797574;
+        const near = 1;
+        const far = 100;
+        scene.fog = new THREE.Fog(color, near, far);
+    }
+    // Adding ground plane:
+    {
+        var geo = new THREE.PlaneBufferGeometry(1000, 1000);
+
+        var groundTexture = new THREE.TextureLoader().load('models/textures/grass.jpg');
+        groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+        groundTexture.repeat.set(500, 500);
+
+        var mat = new THREE.MeshBasicMaterial({ map: groundTexture });
+        var plane = new THREE.Mesh(geo, mat);
+        plane.rotateX(- Math.PI / 2); // rotate it
+        plane.position.y -= 1.;
+
+        scene.add(plane);
     }
 
     function render() {
